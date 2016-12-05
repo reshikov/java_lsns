9bed2c6b0eda7db2c577a4c14107918c08041207
Igor Reshchikov <i.reshikov@gmail.com>
2016-12-06 01:56:08 +0200

    Создал первый тест на Селениум.Билдер
    Ошибка при запуске тестов:
    java.lang.IllegalStateException: The path to the driver executable must
    be set by the webdriver.gecko.driver system property; for more
    information, see https://github.com/mozilla/geckodriver. The latest
    version can be downloaded from
    https://github.com/mozilla/geckodriver/releases

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,9 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-
+    /**
+     первое задание первого урока
+     */
     public static void main(String[] args) {
         System.out.println("Where's my vodka, barman?!");
     }
